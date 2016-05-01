80bab1d917739534d04583e8071ad16ad5061472
weizhaoy <weizhaoy@163.com>
2016-05-01 16:03:39 +0800

    add getter of IASTDeclaraton list

--- cdtDemo/src/com/weizhaoy/cdtdemo/ast/MyASTVisitor.java
+++ cdtDemo/src/com/weizhaoy/cdtdemo/ast/MyASTVisitor.java
@@ -8,11 +8,19 @@ import org.eclipse.cdt.core.dom.ast.IASTDeclaration;
 
 public class MyASTVisitor extends ASTVisitor {
 
-	List<IASTDeclaration> decls =  new ArrayList<>();
+	private List<IASTDeclaration> decls =  new ArrayList<>();
 	@Override
 	public int visit(IASTDeclaration declaration) {
 		// TODO Auto-generated method stub
 		decls.add(declaration);
 		return super.visit(declaration);
 	}
+	/**
+	 * @return the decls
+	 */
+	public List<IASTDeclaration> getDecls() {
+		return decls;
+	}
+	
+	
 }
