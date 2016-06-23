8e72a1b94acd726b9b687db84672721640c33183
qwordy <iamyfy@163.com>
2016-06-23 12:03:58 +0800

    db

--- src/C++/Iutcpp/src/edu/sjtu/stap/iut/LogParserCoverage.java
+++ src/C++/Iutcpp/src/edu/sjtu/stap/iut/LogParserCoverage.java
@@ -1,12 +1,26 @@
 package edu.sjtu.stap.iut;
 
 import java.io.BufferedReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
 /**
  * Created by yfy on 6/22/16.
  * LogParserCoverage
  */
 public class LogParserCoverage implements ITaskAfterRun {
+
+  private Connection conn;
+
+  public LogParserCoverage() throws Exception {
+    conn = DriverManager.getConnection("jdbc:sqlite:coverage.db");
+    Statement stmt = conn.createStatement();
+    String sql = "";
+    stmt.executeUpdate(sql);
+    stmt.close();
+  }
+
   @Override
   public void run(BufferedReader br) throws Exception {
     String testcase, line, func;
