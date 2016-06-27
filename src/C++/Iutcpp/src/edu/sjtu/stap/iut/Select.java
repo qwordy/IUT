11c53fa45df5bd50f6a52ea81748c493d6914b09
qwordy <iamyfy@163.com>
2016-06-27 21:22:43 +0800

    select from db

--- src/C++/Iutcpp/src/edu/sjtu/stap/iut/Select.java
+++ src/C++/Iutcpp/src/edu/sjtu/stap/iut/Select.java
@@ -5,6 +5,8 @@ import edu.sjtu.stap.config.Config;
 import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Set;
 
 /**
@@ -15,5 +17,23 @@ public class Select {
   public static void select(Set<String> diffFuncs) throws Exception {
     String dbFile = Config.getBaseDirInst() + File.separatorChar + "coverage.db";
     Connection conn = DriverManager.getConnection("jdbc:sqlite:" + dbFile);
+    Statement stmt = conn.createStatement();
+    Statement stmt2 = conn.createStatement();
+    String sql = "select test, testCase from cov group by test, testCase;";
+    ResultSet rs = stmt.executeQuery(sql);
+    while (rs.next()) {
+      String test = rs.getString("test");
+      String testCase = rs.getString("testCase");
+      System.out.println(test + ' ' + testCase);
+
+      sql = "select func from cov where test = \"" +
+          test + "\" and testCase = \"" + testCase + "\";";
+      ResultSet rs2 = stmt2.executeQuery(sql);
+      while (rs2.next()) {
+        String func = rs2.getString("func");
+        System.out.println("  " + func);
+      }
+
+    }
   }
 }
